0aad62e6efbffd36cd10992d5a75bdf8557ce11b
alexpant0 <alexandrospantelios1@gmail.com>
2026-01-15 03:10:11 +0200

    Delete src directory

--- src/main/java/org/MY_APP/main/repository/QuizAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.MY_APP.main.repository;
-
-import org.MY_APP.main.model.QuizAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Long> {
-}
