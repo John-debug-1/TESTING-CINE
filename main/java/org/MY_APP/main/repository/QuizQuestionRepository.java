740444aa0ed2d611417d61acfcff200dda293a43
alexpant0 <alexandrospantelios1@gmail.com>
2026-01-15 03:09:26 +0200

    Delete main directory

--- main/java/org/MY_APP/main/repository/QuizQuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.MY_APP.main.repository;
-
-import org.MY_APP.main.model.QuizQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long> {
-}
